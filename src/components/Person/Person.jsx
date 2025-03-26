3a987584f7b9af4d07bc4d16f4a6f2be70c14572
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2025-03-26 14:10:19 +0200

    add function expression messagePartner

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,46 +1,18 @@
 import './Person.scss';
 
+const messagePartner = ({ person }) => {
+  return person.sex === 'm'
+    ? `${person.partnerName} is my wife`
+    : `${person.partnerName} is my husband`;
+};
+
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
     {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
     <p className="Person__partner">
       {!person.isMarried && 'I am not married'}
-      {person.isMarried &&
-        (person.sex === 'm'
-          ? `${person.partnerName} is my wife`
-          : `${person.partnerName} is my husband`)}
+      {person.isMarried && messagePartner({ person })}
     </p>
   </section>
 );
-
-/* export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <div className="Person__partner">
-      {!person.isMarried && <p>I am not married</p>}
-      {person.isMarried &&
-        (person.sex === 'm' ? (
-          <p>{`${person.partnerName} is my wife`}</p>
-        ) : (
-          <p>{`${person.partnerName} is my husband`}</p>
-        ))}
-    </div>
-  </section>
-);
- */
-
-/* export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    {person.isMarried &&
-      (person.sex === 'm' ? (
-        <p>{`${person.partnerName} is my wife`}</p>
-      ) : (
-        <p>{`${person.partnerName} is my husband`}</p>
-      ))}
-  </section>
-);
- */
